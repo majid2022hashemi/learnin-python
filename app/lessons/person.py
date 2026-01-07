44da950afc0d34da6cad8b1e415696d14c7459bf
MajidHashemi <majid2022hashemi@gmail.com>
2026-01-07 16:03:10 +0330

    subclass and override method with super

--- app/lessons/person.py
+++ app/lessons/person.py
@@ -5,16 +5,26 @@ class Person:
         self.pay  = pay
     def lastName(self):
         return self.name.split()[-1]
+    
     def giveRaise(self, percent):
         self.pay = int(self.pay * (1 + percent))
+
     def __repr__(self):                                      # Added method
         return f'[Person: {self.name} ${self.pay:,}]'        # String to print
+    
+class Manager(Person):
+    def giveRaise(self, percent, bonus=.10):
+        super().giveRaise(percent + bonus)
+
 
 if __name__ == '__main__':
-    bob = Person('Bob Smith')
-    sue = Person('Sue Jones', job='dev', pay=100000)
+ 
+    bob = Person('Bob', pay=100)
+
+    tom = Manager('Tom', pay=100)
+
+    bob.giveRaise(.10)
+    tom.giveRaise(.10)
+
     print(bob)
-    print(sue)
-    print(bob.lastName(), sue.lastName())
-    sue.giveRaise(.10)
-    print(sue)
+    print(tom)
