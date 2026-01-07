abf03e2946ebfcb8673404935cd12715d7afe77e
MajidHashemi <majid2022hashemi@gmail.com>
2026-01-07 14:34:52 +0330

    Providing Print Displays

--- app/lessons/person.py
+++ app/lessons/person.py
@@ -3,16 +3,18 @@ class Person:
         self.name = name
         self.job  = job
         self.pay  = pay
-    def lastName(self):                               # Behavior methods
-        return self.name.split()[-1]                  # self is implied subject
+    def lastName(self):
+        return self.name.split()[-1]
     def giveRaise(self, percent):
-        self.pay = int(self.pay * (1 + percent))      # Must change here only
+        self.pay = int(self.pay * (1 + percent))
+    def __repr__(self):                                      # Added method
+        return f'[Person: {self.name} ${self.pay:,}]'        # String to print
 
 if __name__ == '__main__':
     bob = Person('Bob Smith')
     sue = Person('Sue Jones', job='dev', pay=100000)
-    print(bob.name, bob.pay)
-    print(sue.name, sue.pay)
-    print(bob.lastName(), sue.lastName())             # Use the new methods
-    sue.giveRaise(.10)                                # instead of hardcoding
-    print(sue.pay)
+    print(bob)
+    print(sue)
+    print(bob.lastName(), sue.lastName())
+    sue.giveRaise(.10)
+    print(sue)
