89061929009586573a14a042907e8b74861e5be2
MajidHashemi <majid2022hashemi@gmail.com>
2026-01-08 16:08:43 +0330

    person-department

--- app/lessons/person.py
+++ app/lessons/person.py
@@ -5,27 +5,26 @@ class Person:
         self.pay  = pay
     def lastName(self):
         return self.name.split()[-1]
-    
     def giveRaise(self, percent):
         self.pay = int(self.pay * (1 + percent))
+    def __repr__(self):
+        return f'[Person: {self.name} ${self.pay:,}]'
 
-    def __repr__(self):                                      # Added method
-        return f'[Person: {self.name} ${self.pay:,}]'        # String to print
 class Manager(Person):
-    def __init__(self, name, pay):
-        super().__init__(name, 'mgr', pay)
-
-
+    def __init__(self, name, pay):                     # Redefine constructor
+        Person.__init__(self, name, 'mgr', pay)        # Run original with 'mgr'
     def giveRaise(self, percent, bonus=.10):
-        super().giveRaise(percent + bonus)
-
+        Person.giveRaise(self, percent + bonus)
 
 if __name__ == '__main__':
- 
-    bob = Person('Bob', pay=100)
-    sue = Person('sue', pay=95)
-    pat = Manager('Pat', pay=100)
-
-    for obj in (bob, sue, pat):
-        obj.giveRaise(.10)
-        print(obj)
+    bob = Person('Bob Smith')
+    sue = Person('Sue Jones', job='dev', pay=100000)
+    print(bob)
+    print(sue)
+    print(bob.lastName(), sue.lastName())
+    sue.giveRaise(.10)
+    print(sue)
+    pat = Manager('Pat Jones', 50000)                   # Job name set by class
+    pat.giveRaise(.10)
+    print(pat.lastName())
+    print(pat)
